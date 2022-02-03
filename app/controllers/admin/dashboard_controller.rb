46271012e17060274076762e4db04cca12710baa
Bita <bita.janzadeh@hotmail.com>
2022-02-03 12:46:54 -0500

    Implement general stats on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   require 'dotenv/load'
   http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
+
   def show
+    @products = Product.all.order(created_at: :desc)
+    @category = Category.all
   end
 end
