13d47502f52ac06f722a71a212f527caf6fd65d5
Bita <bita.janzadeh@hotmail.com>
2022-02-03 01:43:31 -0500

    Password protect admin features

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  require 'dotenv/load'
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  
   def index
     @products = Product.order(id: :desc).all
   end
