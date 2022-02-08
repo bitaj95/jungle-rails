1cf6fef07f0eaf998b130abd47b06e776be90fa1
Bita <bita.janzadeh@hotmail.com>
2022-02-07 21:34:53 -0500

    Add password_confirmation validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password, :password_confimation
+  validates :password, length: {minimum: 8}
 end
