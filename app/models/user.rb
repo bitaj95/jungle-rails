aae92996b4bd3a84566a6e896b4c5ed9e0f7a558
Bita <bita.janzadeh@hotmail.com>
2022-02-07 23:52:14 -0500

    Write tests for the user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name, :email, :password, :password_confirmation
   validates :password, length: {minimum: 8}
+
+
+  # Check if the user exists and if the password correct
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email).try(:authenticate, password)
+    if @user
+      @user
+    else
+      nil
+    end
+  end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,7 +42,31 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages.first).to eq "Password is too short (minimum is 8 characters)"
     end
+  end
 
+  describe '.authenticate_with_credentials' do
+    it 'is valid with the correct attributes' do
+      @user = User.new(name: "Arthur", email: "test@mail.com", password: "Aardvark123", password_confirmation: "Aardvark123")
+      expect(@user).to be_valid
+      @user.save
+      @user2 = User.authenticate_with_credentials('test@mail.com', 'Aardvark123')
+      expect(@user2).to_not be nil
+    end
+    
+    it 'is not valid with a non-existing email' do
+      @user = User.new(name: "Arthur", email: "test@mail.com", password: "Aardvark123", password_confirmation: "Aardvark123")
+      expect(@user).to be_valid
+      @user.save
+      @user2 = User.authenticate_with_credentials('different-email@mail.com', 'Aardvark123')
+      expect(@user2).to be nil
+    end
 
+    it 'is not valid with the wrong password' do
+      @user = User.new(name: "Arthur", email: "test@mail.com", password: "Aardvark123", password_confirmation: "Aardvark123")
+      expect(@user).to be_valid
+      @user.save
+      @user2 = User.authenticate_with_credentials('test@mail.com', 'Aard123')
+      expect(@user2).to be nil
+    end
   end
 end
